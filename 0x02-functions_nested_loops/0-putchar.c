0283258f8f4a3d6c2cdf10e09e9c4d3fda03f41f
fkgirma <fikriegirma@gmail.com>
2022-09-19 18:00:13 -0700

    task 01

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * A program that prints out _putchar
+ * main - Entry point
+ * Return - Always 0
  */
+
 int main(void)
 {
-	printf("_putchar\n");
+	char c[5] = "hello";
+	int i;
+	for  (i = 0; i <5; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+
+
 }
