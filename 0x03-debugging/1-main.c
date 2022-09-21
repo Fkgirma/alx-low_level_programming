526f935b2e21f0ed9d0db30a7b781452ff92bc2a
fkgirma <fikriegirma@gmail.com>
2022-09-21 13:04:05 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,15 +7,18 @@
 int main(void)
 {
 	int i;
-	/*white space*/
+	/*white erro occur space*/
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 	/*
 	 * while (i < 10)
-	 * {
-	 * putchar(i);
+	 *  {
+	 *   putchar(i);
 	 * }
+	 * s
+	 * s
 	 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
+	/*end of the pro*/
 }
