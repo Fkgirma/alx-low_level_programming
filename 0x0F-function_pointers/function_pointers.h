9b397761302f20341cc8227fb845e4a214c1b7e8
fkgirma <fikriegirma@gmail.com>
2022-10-18 06:11:49 -0700

    header and first

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,3 @@
-#ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
 #include <stdio.h>
@@ -9,4 +8,3 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /*MAIN_H*/
-
